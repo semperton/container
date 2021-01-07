25ba2bc194d15e46edbf77faf99b0a22a79a28a1
Johannes Rabausch <jrabausch@absatzformat.de>
2021-01-07 11:01:55 +0100

    Some more tests

--- tests/index.php
+++ tests/index.php
@@ -12,30 +12,43 @@ $container = new Container();
 class A
 {
 	public $name;
-	public function __construct(string $name)
+	private $active;
+	public function __construct(string $name, bool $active = true)
 	{
 		$this->name = $name;
+		$this->active = $active;
 	}
 }
 
 class B
 {
+	protected $salute;
 	protected $a;
-	public function __construct(A $a)
+	public function __construct(string $salute, A $a)
 	{
+		$this->salute = $salute;
 		$this->a = $a;
 	}
-	public function hello()
+	public function hello(): string
 	{
-		echo $this->a->name;
+		return $this->salute . ' ' . $this->a->name;
 	}
 }
 
 // $container->{A::class} = new A('test');
+
 $container->name = 'Semperton';
+
 $container->{A::class} = function (ContainerInterface $c) {
+
 	$name = $c->get('name');
 	return new A($name);
 };
 
-$container->get(B::class)->hello();
+$container->set(B::class, function (ContainerInterface $c) {
+
+	$a = $c->get(A::class);
+	return new B('Hello', $a);
+});
+
+echo $container->get(B::class)->hello();
